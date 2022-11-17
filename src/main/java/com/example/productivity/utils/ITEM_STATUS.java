d25551a0f1bc03ce4ec4c854c4bcdfb6a56e6932
Mfitumukiza Peter <mfitep6@gmail.com>
2022-11-17 12:12:05 +0200

    added models

---
+++ src/main/java/com/example/productivity/utils/ITEM_STATUS.java
@@ -0,0 +1,6 @@
+package com.example.productivity.utils;
+
+public enum PLAN_STATUS {
+    DONE,
+    IN_PROGRESS
+}
---
+++ src/main/java/com/example/productivity/utils/UserValidator.java
@@ -0,0 +1,2 @@
+package com.example.productivity.utils;public class UserValidator {
+}
